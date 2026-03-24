84aefe4d43a952c722767f5280c87c818e2bfb5c
jia <1159063863@qq.com>
2026-03-24 17:58:17 +0800

    Updates

---
+++ assets/Blog-BFvNW2KV.js
@@ -0,0 +1,1 @@
+import{E as e,It as t,U as n,v as r,x as i}from"./vue.runtime.esm-bundler-SuLFlfFu.js";var a={class:`p-8`},o={class:`text-2xl font-bold`},s=e({name:`Blog`,__name:`Blog`,setup(e){return(e,s)=>(n(),i(`div`,a,[r(`h1`,o,t(e.$t(`nav.blog`)),1),s[0]||=r(`p`,{class:`mt-4 text-slate-600 dark:text-slate-400`},`Placeholder for Blog page Content.`,-1)]))}});export{s as default};
